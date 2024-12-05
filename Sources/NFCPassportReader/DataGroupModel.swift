074c88574baca6208ace0c60b9faf2aec14fd65f
Hoàng Chiều <hoangchieukma@gmail.com>
2024-12-05 11:59:39 +0700

    Init code

---
+++ Sources/NFCPassportReader/DataGroupModel.swift
@@ -0,0 +1,17 @@
+//
+//  DataGroupModel.swift
+//  NFCPassportReader
+//
+//  Created by Hoang Van Chieu on 17/9/24.
+//
+
+import Foundation
+public struct DataGroupModel : Codable{
+    var COM: String = ""
+    var SOD: String = ""
+    var DG13: String = ""
+    var DG14: String = ""
+    var DG2: String = ""
+    var DG1: String = ""
+    var DG15: String = ""
+}
